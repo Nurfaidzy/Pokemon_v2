a1cdf00023241fd574aebcdc983b69e471139d61
nurfaidzy <nurfaidzy@gmail.com>
2022-09-22 23:35:14 +0700

    Checkpoint Nabar n Card

--- src/Component/HomePage/Card.jsx
+++ src/Component/HomePage/Card.jsx
@@ -10,21 +10,43 @@ const Card = ({ data }) => {
     dispatch(kirim_detail(props));
     navigate("/detail");
   };
+  console.log(data);
   return (
     <div className="py-[5%]">
-      <div className="text-center uppercase font-bold text-2xl py-10">
-        ini datanya
+      <div className="text-center uppercase font-bold text-4xl text-yellow-500 py-20">
+        Pokemon List
       </div>
       <div className="flex justify-center">
-        <div className="grid grid-cols-6 gap-40">
+        <div className="grid grid-cols-3 gap-20 ">
           {data.asetpoke.map((x, i) => (
-            <div key={i}>
+            <div
+              key={i}
+              className="bg-yellow-200/20 shadow-lg p-8 rounded-lg flex"
+            >
               <div className="flex justify-center">
-                <img src={x.sprites.front_default} alt={x.name} />
+                <img
+                  src={x.sprites.front_default}
+                  className="w-[100px] h-fit"
+                  alt={x.name}
+                />
               </div>
-              <div className="text-center uppercase">{x.name}</div>
-              <div className="text-center" onClick={() => pindah(x.name)}>
-                Detail
+              <div className="grid grid-cols-1 gap-1">
+                <div className="text-center uppercase font-bold text-xl ">
+                  {x.name}
+                </div>
+                <div className="text-center">Type</div>
+                <div className="grid grid-cols-2 gap-4">
+                  {x.types.map((y, j) => (
+                    <div key={j} className="text-center capitalize">
+                      <div>{y.type.name}</div>
+                    </div>
+                  ))}
+                </div>
+                <div className="text-center" onClick={() => pindah(x.name)}>
+                  <button className="py-1 px-2 rounded-lg  bg-yellow-300">
+                    Detail
+                  </button>
+                </div>
               </div>
             </div>
           ))}
--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,8 +1,17 @@
+import { FaGithub } from "react-icons/fa";
 const Navbar = () => {
   return (
     <div className="fixed w-full">
-      <div className="py-10 bg-white shadow-lg px-[5%]">
+      <div className="py-10 bg-white shadow-lg px-[5%] flex justify-between bg-white/60 backdrop-blur-lg">
         <div className="font-bold text-2xl">Pokemon</div>
+        <div>
+          <a href="https://github.com/Nurfaidzy" className="flex gap-2">
+            <div className="pt-1">
+              <FaGithub />
+            </div>
+            <div>Mygithub</div>
+          </a>
+        </div>
       </div>
     </div>
   );
