80fc48127c16b98dfbe32796e0b2714b1af06aa5
nurfaidzy <nurfaidzy@gmail.com>
2022-09-23 12:49:45 +0700

    pebaikan pada card style

--- src/Component/HomePage/Card.jsx
+++ src/Component/HomePage/Card.jsx
@@ -12,7 +12,7 @@ const Card = ({ data }) => {
   };
   console.log(data);
   return (
-    <div className="py-[5%]">
+    <div className="py-[5%] bg-cover">
       <div className="text-center uppercase font-bold text-4xl text-yellow-500 py-20">
         Pokemon List
       </div>
@@ -21,17 +21,21 @@ const Card = ({ data }) => {
           {data.asetpoke.map((x, i) => (
             <div
               key={i}
-              className="bg-yellow-200/20 shadow-lg p-8 rounded-lg flex"
+              className=" hover:shadow-xl p-8 rounded-lg flex gap-5 bg-cover   "
+              style={{
+                backgroundImage:
+                  "url(https://i.pinimg.com/564x/90/02/46/9002463f0951e6915d4e30b29f90a002.jpg)",
+              }}
             >
-              <div className="flex justify-center">
+              <div className="flex justify-center pt-5 ">
                 <img
                   src={x.sprites.front_default}
-                  className="w-[100px] h-fit "
+                  className="w-[100px] h-fit bg-white rounded-full shadow-lg "
                   alt={x.name}
                 />
               </div>
-              <div className="grid grid-cols-1 gap-1">
-                <div className="text-center uppercase font-bold text-xl ">
+              <div className="grid grid-cols-1 gap-1  px-5 py-2 rounded-lg shadow-lg bg-white">
+                <div className="text-center capitalize font-bold text-xl ">
                   {x.name}
                 </div>
                 <div className="text-center">Type</div>
